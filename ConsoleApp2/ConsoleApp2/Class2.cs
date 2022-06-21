42a3cf49490a1c42a95952dbda53523d9c9d417f
Aisharya28071995 <gaishwarya58@gmail.com>
2022-06-21 19:22:37 +0530

    commiting linkedlistfiles

---
+++ ConsoleApp2/ConsoleApp2/Class2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class LinkedList
+    {
+        public Node head;
+
+        public bool Add(int data)
+        {
+            Node node = new Node(data);
+
+            if(head == null)
+            {
+                head = node;
+            }
+            node.next = head;
+            head = node;
+            return true;
+
+        }
+
+        public bool Append(int data)
+        {
+            Node node = new Node(data);
+
+            if(head == null)
+            {
+                head = node;
+            }
+
+            Node t = head;
+
+            while(t.next!=null)
+            {
+                t = t.next;
+
+            }
+            t.next = node;
+            return true;
+        }
+
+        public void Display()
+        {
+            Node t = head;
+
+            while(t!=null)
+            {
+                Console.WriteLine(t.data);
+                t = t.next;
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(20);
+            linkedList.Add(56);
+
+            linkedList.Display();
+        }
+    }
+}
